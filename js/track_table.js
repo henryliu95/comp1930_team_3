57b653fb025219c5916692ca715f98d5a63ca61f
Henry Liu <henryliu95@hotmail.com>
2019-03-31 00:13:49 -0700

    Checks if user is logged in

--- js/track_table.js
+++ js/track_table.js
@@ -41,3 +41,18 @@ function deleteRow(r) {
     let i = r.parentNode.parentNode.rowIndex;
     document.getElementById("groceryTable").deleteRow(i);
 }
+
+function change_login(){
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        console.log("below is the UID of the logged in storage.")
+        console.log(user.uid)
+        document.getElementById("login").innerHTML = "SIGN OUT";
+      } else {
+        // No user is signed in.
+        console.log("no user signed in at the moment")
+      }
+    });
+            }
+change_login();
