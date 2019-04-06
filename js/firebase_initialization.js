85c424c40666a08a5ebf6701d9bf1cb319e5943b
Geoff Hui <geoffhui5@gmail.com>
2019-04-05 23:22:56 -0700

    separated_js_files

---
+++ js/firebase_initialization.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyC79Hh-cfEC11x5_p5XLI2hlkXZUUQSooo",
+authDomain: "keep-it-fresh-ddf5f.firebaseapp.com",
+databaseURL: "https://keep-it-fresh-ddf5f.firebaseio.com",
+projectId: "keep-it-fresh-ddf5f",
+storageBucket: "keep-it-fresh-ddf5f.appspot.com",
+messagingSenderId: "935147192047"
+};
+firebase.initializeApp(config);
